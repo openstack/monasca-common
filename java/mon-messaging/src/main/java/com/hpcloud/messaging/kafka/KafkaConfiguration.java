527c253b2f8dde4193e3ec6785af5a942bfc20e0
Jonathan Halterman <jhalterman@gmail.com>
2014-02-20 15:32:49 -0800

    Basic KafkaService

--- java/mon-messaging/src/main/java/com/hpcloud/messaging/kafka/KafkaConfiguration.java
+++ java/mon-messaging/src/main/java/com/hpcloud/messaging/kafka/KafkaConfiguration.java
@@ -1,16 +1,11 @@
 package com.hpcloud.messaging.kafka;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-
 import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * @author Jonathan Halterman
  */
 public class KafkaConfiguration {
-  @NotEmpty public String[] hosts;
-  @Min(1) @Max(65535) public int port = 5672;
-  @NotEmpty public String username;
-  @NotEmpty public String password;
+  @NotEmpty public String[] zookeeperUris;
+  @NotEmpty public String[] brokerUris;
 }
