10e36da2a571eb4569ad67f03eb36b6d48e59fef
Jonathan Halterman <jhalterman@gmail.com>
2014-03-10 13:53:09 -0700

    Removed headers from TupleDeserializer

--- java/mon-streaming/src/main/java/com/hpcloud/streaming/storm/TupleDeserializer.java
+++ java/mon-streaming/src/main/java/com/hpcloud/streaming/storm/TupleDeserializer.java
@@ -1,7 +1,6 @@
 package com.hpcloud.streaming.storm;
 
 import java.util.List;
-import java.util.Map;
 
 import backtype.storm.tuple.Fields;
 
@@ -16,7 +15,7 @@ public interface TupleDeserializer {
    * Returns a list of deserialized tuples, consisting of a list of tuples each with a list of
    * fields, for the {@code tuple}, else null if the {@code tuple} cannot be deserialized.
    */
-  List<List<?>> deserialize(byte[] tuple, Map<String, Object> headers);
+  List<List<?>> deserialize(byte[] tuple);
 
   /**
    * Returns the output fields.
