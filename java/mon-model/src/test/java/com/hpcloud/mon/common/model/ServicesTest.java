669469f61d31a58ac4b9bbc11ba5bdb4615b8298
Jonathan Halterman <jhalterman@gmail.com>
2014-03-20 13:14:13 -0700

    NamespacesTest -> ServicesTest

--- java/mon-model/src/test/java/com/hpcloud/mon/common/model/ServicesTest.java
+++ java/mon-model/src/test/java/com/hpcloud/mon/common/model/ServicesTest.java
@@ -2,17 +2,10 @@ package com.hpcloud.mon.common.model;
 
 import static org.testng.Assert.assertTrue;
 
-import java.util.List;
-
 import org.testng.annotations.Test;
 
 @Test
-public class NamespacesTest {
-  public void shouldReturnEmptyForRequiredUserDefinedDimensions() {
-    List<String> list = Services.getRequiredDimensions("userdefined");
-    assertTrue(list.isEmpty());
-  }
-
+public class ServicesTest {
   public void shouldValidateObjectStoreMetricName() {
     assertTrue(Services.isValidMetricName("hpcs.object-store", "project_write_bytes"));
   }
