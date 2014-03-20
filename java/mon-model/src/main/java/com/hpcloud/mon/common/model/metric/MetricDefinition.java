7ab04682a11ce1e9942a46df1b4ebd2cd85e6c5a
Jonathan Halterman <jhalterman@gmail.com>
2014-03-20 16:36:15 -0700

    Removed unused method from MetricDefinition.

--- java/mon-model/src/main/java/com/hpcloud/mon/common/model/metric/MetricDefinition.java
+++ java/mon-model/src/main/java/com/hpcloud/mon/common/model/metric/MetricDefinition.java
@@ -2,15 +2,10 @@ package com.hpcloud.mon.common.model.metric;
 
 import java.io.Serializable;
 import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 import javax.annotation.Nullable;
 
-import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
-import com.google.common.hash.HashCode;
-import com.google.common.hash.Hashing;
 
 /**
  * Metric definition.
@@ -75,34 +70,6 @@ public class MetricDefinition implements Serializable {
     if (dimensions != null)
       b.append(dimensions);
     return b.toString();
-  }
-
-  /**
-   * Returns a string represents a hash of the name + dimensions. Convert that string to a UTF-8
-   * character encoded language neutral UUID. This encoding must be reproduceable in other
-   * languages. Uses 128 bit MD5.
-   * 
-   * @return MD5 128 bit hash of name + ordered dimensions
-   */
-  public HashCode toHashCode() {
-    // Lazily sort dimensions
-    if (!(dimensions instanceof SortedMap))
-      dimensions = new TreeMap<>(dimensions);
-
-    StringBuilder sb = new StringBuilder(name);
-    sb.append('=');
-    if (dimensions != null) {
-      boolean first = true;
-      for (Map.Entry<String, String> dimension : dimensions.entrySet()) {
-        if (!first) {
-          sb.append(':');
-          first = false;
-        }
-        sb.append(dimension.getKey()).append(':').append(dimension.getValue());
-      }
-    }
-
-    return Hashing.md5().newHasher().putString(sb.toString(), Charsets.UTF_8).hash();
   }
 
   @Override
