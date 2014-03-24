cf35d34f3b49d8ad915e101ea03b18f18fc00563
Jonathan Halterman <jhalterman@gmail.com>
2014-03-24 11:17:56 -0700

    Added AlarmUpdatedEvent

---
+++ java/mon-model/src/main/java/com/hpcloud/mon/common/event/AlarmUpdatedEvent.java
@@ -0,0 +1,102 @@
+package com.hpcloud.mon.common.event;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.hpcloud.mon.common.model.alarm.AlarmSubExpression;
+
+/**
+ * Represents an alarm having been updated.
+ * 
+ * @author Jonathan Halterman
+ */
+@JsonRootName(value = "alarm-updated")
+public class AlarmUpdatedEvent implements Serializable {
+  private static final long serialVersionUID = -8687756708588262533L;
+
+  public String tenantId;
+  public String alarmId;
+  public String alarmName;
+  public String alarmExpression;
+  public Map<String, AlarmSubExpression> oldAlarmSubExpressions;
+  public Map<String, AlarmSubExpression> newAlarmSubExpressions;
+
+  public AlarmUpdatedEvent() {
+  }
+
+  public AlarmUpdatedEvent(String tenantId, String alarmId, String alarmName,
+      String alarmExpression, Map<String, AlarmSubExpression> oldAlarmSubExpressions,
+      Map<String, AlarmSubExpression> newAlarmSubExpressions) {
+    this.tenantId = tenantId;
+    this.alarmId = alarmId;
+    this.alarmName = alarmName;
+    this.alarmExpression = alarmExpression;
+    this.oldAlarmSubExpressions = oldAlarmSubExpressions;
+    this.newAlarmSubExpressions = newAlarmSubExpressions;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    AlarmUpdatedEvent other = (AlarmUpdatedEvent) obj;
+    if (alarmExpression == null) {
+      if (other.alarmExpression != null)
+        return false;
+    } else if (!alarmExpression.equals(other.alarmExpression))
+      return false;
+    if (alarmId == null) {
+      if (other.alarmId != null)
+        return false;
+    } else if (!alarmId.equals(other.alarmId))
+      return false;
+    if (alarmName == null) {
+      if (other.alarmName != null)
+        return false;
+    } else if (!alarmName.equals(other.alarmName))
+      return false;
+    if (newAlarmSubExpressions == null) {
+      if (other.newAlarmSubExpressions != null)
+        return false;
+    } else if (!newAlarmSubExpressions.equals(other.newAlarmSubExpressions))
+      return false;
+    if (oldAlarmSubExpressions == null) {
+      if (other.oldAlarmSubExpressions != null)
+        return false;
+    } else if (!oldAlarmSubExpressions.equals(other.oldAlarmSubExpressions))
+      return false;
+    if (tenantId == null) {
+      if (other.tenantId != null)
+        return false;
+    } else if (!tenantId.equals(other.tenantId))
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((alarmExpression == null) ? 0 : alarmExpression.hashCode());
+    result = prime * result + ((alarmId == null) ? 0 : alarmId.hashCode());
+    result = prime * result + ((alarmName == null) ? 0 : alarmName.hashCode());
+    result = prime * result
+        + ((newAlarmSubExpressions == null) ? 0 : newAlarmSubExpressions.hashCode());
+    result = prime * result
+        + ((oldAlarmSubExpressions == null) ? 0 : oldAlarmSubExpressions.hashCode());
+    result = prime * result + ((tenantId == null) ? 0 : tenantId.hashCode());
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "AlarmUpdatedEvent [tenantId=%s, alarmId=%s, alarmName=%s, expression=%s]", tenantId,
+        alarmId, alarmName, alarmExpression);
+  }
+}
