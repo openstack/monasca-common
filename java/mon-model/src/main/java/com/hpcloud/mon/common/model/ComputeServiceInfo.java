9c68beb155307cd803fd11a3cfe487841b862e69
Jonathan Halterman <jhalterman@gmail.com>
2014-03-24 11:29:52 -0700

    cpu_total_time -> cpu_time, cpu_total_utilization -> cpu_utilization

--- java/mon-model/src/main/java/com/hpcloud/mon/common/model/ComputeServiceInfo.java
+++ java/mon-model/src/main/java/com/hpcloud/mon/common/model/ComputeServiceInfo.java
@@ -12,14 +12,13 @@ import com.google.common.collect.Sets;
  * @author Jonathan Halterman
  */
 class ComputeServiceInfo implements ServiceInfo {
-  private final Set<String> supportedMetricNames = Sets.newHashSet("cpu_total_time",
-      "cpu_total_utilization", "disk_read_ops", "disk_read_ops_count", "disk_write_ops",
-      "disk_write_ops_count", "disk_read_bytes", "disk_read_bytes_count", "disk_write_bytes",
-      "disk_write_bytes_count", "net_in_bytes", "net_in_bytes_count", "net_out_bytes",
-      "net_out_bytes_count", "net_in_packets", "net_in_packets_count", "net_out_packets",
-      "net_out_packets_count", "net_in_dropped", "net_in_dropped_count", "net_out_dropped",
-      "net_out_dropped_count", "net_in_errors", "net_in_errors_count", "net_out_errors",
-      "net_out_errors_count");
+  private final Set<String> supportedMetricNames = Sets.newHashSet("cpu_time", "cpu_utilization",
+      "disk_read_ops", "disk_read_ops_count", "disk_write_ops", "disk_write_ops_count",
+      "disk_read_bytes", "disk_read_bytes_count", "disk_write_bytes", "disk_write_bytes_count",
+      "net_in_bytes", "net_in_bytes_count", "net_out_bytes", "net_out_bytes_count",
+      "net_in_packets", "net_in_packets_count", "net_out_packets", "net_out_packets_count",
+      "net_in_dropped", "net_in_dropped_count", "net_out_dropped", "net_out_dropped_count",
+      "net_in_errors", "net_in_errors_count", "net_out_errors", "net_out_errors_count");
   private final List<String> supportedDims = Arrays.asList("instance_id");
 
   @Override
