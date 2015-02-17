022d1a5a40164b410d87168a8d63c4aea21b50f9
Craig Bryant <craig.bryant@hp.com>
2015-02-17 15:26:07 -0700

    Add missing copyrights
    Change-Id: If7720d36d6a229ecf0084c1561a6a4dc9fff16d8

--- java/monasca-common-middleware/src/main/java/monasca/common/middleware/AuthConstants.java
+++ java/monasca-common-middleware/src/main/java/monasca/common/middleware/AuthConstants.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package monasca.common.middleware;
 
 public interface AuthConstants {
@@ -56,10 +69,10 @@ public interface AuthConstants {
    * Auth server port: 9543 for Thrift, 35357 for HTTP.
    */
   public static final String SERVER_PORT = "ServerPort";
-  /**
-   * Use https to connect to SERVER_VIP and SERVER_PORT if true.
-   */
-  public static final String USE_HTTPS = "UseHttps";
+  /*
+   * Use https to connect to SERVER_VIP and SERVER_PORT if true
+   *
+  public static final String USE_HTTPS = "UseHttps"
   /**
    * connection timeout initialization parameter
    */
