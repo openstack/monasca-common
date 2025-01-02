a723f69d59fe0ac2dbe3d0ba31b104e309f2e61b
Hoai-Thu Vuong <thuvh87@gmail.com>
2025-01-02 14:37:07 +0700

    upgrade monasca
    - upgrade maven and use https repo
    - temporary disable some jobs
    - upgrade librdkafka
    
    Change-Id: Ifaecf9039581cd3851b47344f71ad2c1c390484d

--- run_maven.sh
+++ run_maven.sh
@@ -2,9 +2,9 @@
 # Download maven 3 if the system maven isn't maven 3
 VERSION=`mvn -v | grep "Apache Maven 3"`
 if [ -z "${VERSION}" ]; then
-   curl http://archive.apache.org/dist/maven/binaries/apache-maven-3.2.1-bin.tar.gz > apache-maven-3.2.1-bin.tar.gz
-   tar -xvzf apache-maven-3.2.1-bin.tar.gz
-   MVN=${PWD}/apache-maven-3.2.1/bin/mvn
+   curl https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz > apache-maven-3.9.9-bin.tar.gz
+   tar -xvzf apache-maven-3.9.9-bin.tar.gz
+   MVN=${PWD}/apache-maven-3.9.9/bin/mvn
 else
    MVN=mvn
 fi
@@ -20,5 +20,5 @@ RC=$?
 mkdir -p target
 cp java/*/target/*.jar target
 
-rm -fr apache-maven-3.2.1*
+rm -fr apache-maven-3.9.9*
 exit $RC
